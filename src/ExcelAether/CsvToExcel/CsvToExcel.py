e2d425d66283ca295c438727c9a20359ebe5edbb
seirios-ls <237472989@qq.com>
2022-12-23 15:30:00 +0800

    csv2xls

--- src/ExcelAether/CsvToExcel/CsvToExcel.py
+++ src/ExcelAether/CsvToExcel/CsvToExcel.py
@@ -1,6 +1,7 @@
 import sys
 import xlwt
 import chardet
+import re
 
 if __name__ == '__main__':
     arge = sys.argv
@@ -15,12 +16,18 @@ if __name__ == '__main__':
     xls = xlwt.Workbook()
     sheet = xls.add_sheet('sheet1', cell_overwrite_ok=True)
     x = 0
+
     while True:
         line = f.readline()
+        line = line.strip()
         if not line:
             break
         for i in range(len(line.split(','))):
             item = line.split(',')[i]
+            # 金额转换
+            if re.search('^(([1-9]{1}\\d*)|([0]{1}))(\\.(\\d){0,2})?$', item):
+                item = float('%.2f' % float(item))
+
             sheet.write(x, i, item)
         x += 1
     f.close()
